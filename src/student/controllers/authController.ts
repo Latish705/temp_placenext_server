479a9947e2dba996894a1f8ad357e22104eb7c8f
Latish705 <latishadwani705@gmail.com>
2025-08-15 20:41:58 +0530

    implemented signup

--- src/student/controllers/authController.ts
+++ src/student/controllers/authController.ts
@@ -7,18 +7,56 @@ export const signin = async (req: Request, res: Response): Promise<any> => {
     //@ts-ignore
     const user = req.user;
     //@ts-ignore
-    const student: Student = await dbStudent.findOne({
+    const dbstud = await dbStudent.findOne({
       firebaseId: user.firebaseId,
     });
 
-    student.signin();
-
+    if (!dbstud) {
+      return res
+        .status(400)
+        .json({ success: false, message: "register first" });
+    }
     return res.status(200).json({
       succes: false,
       message: "Sigin is successful ",
     });
   } catch (error: any) {
     console.log("Error in signin: ", error.message);
-    res.status(500).send("Internal Server Error");
+    res.status(500).json({ success: false, message: "Internal Server Error" });
+  }
+};
+
+export const signup = async (req: Request, res: Response): Promise<any> => {
+  try {
+    // @ts-ignore
+    const user = req.user;
+    const dbStud = await dbStudent.findOne({ firebaseId: user.firebaseId });
+    if (!dbStud) {
+      return res.status(200).json({ success: true, isFirsLogin: false });
+    }
+    return res.status(200).json({ success: true, isFirsLogin: true });
+  } catch (error: any) {
+    console.log("Error in singup: ", error.message);
+    res.status(500).json({
+      success: false,
+      message: "Internal Server Error",
+    });
+  }
+};
+
+export const applicationForm = async (
+  req: Request,
+  res: Response
+): Promise<any> => {
+  try {
+    // @ts-ignore
+    const user = req.user;
+    const bo = req.body;
+  } catch (error: any) {
+    console.log("Error in Application Form: ", error.message);
+    res.status(500).json({
+      success: false,
+      message: "Internal Server Error",
+    });
   }
 };
--- src/student/strategies/StudentAuth.ts
+++ src/student/strategies/StudentAuth.ts
@@ -1,7 +1,7 @@
 import { User } from "../../model/User";
 import { AuthStrategy } from "../../strategies/AuthStrategy";
 
-class StudentAuth implements AuthStrategy {
+export class StudentAuth implements AuthStrategy {
   signin(user: User): boolean {
     return true;
   }
